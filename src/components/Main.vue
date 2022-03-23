d030444d868238c78f601fb6bf25e533ad1fe321
MaryahKim <maryapitaz@gmail.com>
2022-03-23 19:49:31 +0300

    Create Main.vue
    Create Main component.
    Created a container of the main page content .

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+ <div class=" grid grid-cols-2 w-screen pb-10">
+      <div class="p-10  "> 
+        <h1 class=" pb-2 font-black text-5xl  text-very-dark-blue">More than just shorter links</h1>
+        <p class="pb-5 text-grayish-violet">Build your brand's recognition and get detailed insights on how your links are performing.</p>
+        <button class="bg-cyan p-3 rounded-3xl text-white text-xl font-bold "><span class="p-4"> Get Started</span></button>
+    </div>
+    <div class="p-1 ">
+        <img src="../assets/images/illustration-working.svg" alt="">
+    </div>
+ </div>
+</template>
