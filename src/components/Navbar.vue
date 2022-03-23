cff5110681367bd4aacdb3978b7646e48b6cf99e
MaryahKim <maryapitaz@gmail.com>
2022-03-23 19:50:47 +0300

    Create Navbar.vue
    Create Navbar component.
    Added navbar elements and styles

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<template>
+  <header>
+      <div class=" font-['Poppins'] weight-medium text-grayish-violet flex p-10">
+        <div class="">
+           <img class="" src="../assets/images/logo.svg" alt="logo">
+        </div>
+        <nav class="">
+          <RouterLink class="p-3 hover:text-very-dark-violet" to='/'>Features</RouterLink>
+          <RouterLink class="p-3 hover:text-very-dark-violet" to= '/pricing' >Pricing</RouterLink>
+          <RouterLink class="p-3 hover:text-very-dark-violet" to= '/resources' >Resources</RouterLink>
+        </nav>
+        <div class="pl-60 flex flex-row items-center">
+            <span class="pr-6">Login</span>
+            <button class="text-zinc-100 text-md px-4 py-2 bg-cyan rounded-3xl">Sign Up</button>
+          </div>
+      </div>
+  </header>
+</template>
+
+
+
