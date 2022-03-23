fc2166328580e297937b759c8c2df3764aef0f0d
MaryahKim <maryapitaz@gmail.com>
2022-03-23 19:53:23 +0300

    Create tailwind.config.js
    Create custom colors, fonts, and Images

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'cyan': 'hsl(180, 66%, 49%)',
+        'dark-violet': 'hsl(257, 27%, 26%)',
+        'gray': 'hsl(0, 0%, 75%)',
+        'grayish-violet': 'hsl(257, 7%, 63%)',
+        'very-dark-blue': 'hsl(255, 11%, 22%)',
+        'very-dark-violet': 'hsl(260, 8%, 14%)'
+      },
+      fonts: {
+       'Poppins': ['Poppins', 'Georgia'],
+      },
+      backgroundImage: {
+        'boost-desktop': "url('/src/assets/images/bg-boost-desktop.svg')",
+        'shorten-desktop': "url('/src/assets/images/bg-shorten-desktop.svg')",
+      },
+    },
+  },
+  plugins: [],
+}
