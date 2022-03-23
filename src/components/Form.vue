f3189c24020e258e85ddbf098362f60baf5c6d6e
MaryahKim <maryapitaz@gmail.com>
2022-03-23 19:46:56 +0300

    Create Form.vue
    Add Form element

---
+++ src/components/Form.vue
@@ -0,0 +1,16 @@
+
+
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <div class="w-1/2 bg-cover  bg-no-repeat  bg-shorten-desktop bg-very-dark-violet p-5 rounded-md flex items-center justify-center m-auto">
+      <form action="">
+        <input class="bg-white" placeholder="Shorten a link here..." type="link">
+        <button class="w-32  rounded-md p-3">Shorten it!</button>
+      </form>
+  </div>
+</template>
