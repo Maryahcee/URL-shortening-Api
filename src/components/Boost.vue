9c79d65b44547396ca1072552aab2aa2250b2981
MaryahKim <maryapitaz@gmail.com>
2022-03-24 18:59:43 +0300

    Create Boost.vue
    Create Boost component.
    Added link with a button

---
+++ src/components/Boost.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card max-w-full">
+    <div class="p-10 max-w-full flex flex-col items-center justify-center bg-shorten-desktop bg-no-repeat  bg-cover bg-dark-violet font-['Poppins']">
+        <h3 class=" text-zinc-200  text-center text-4xl font-extrabold pb-5 ">Boost your links today</h3>
+        <button class="bg-cyan text-zinc-200 text-2xl font-semibold  rounded-full py-3 px-6">Get Started</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
