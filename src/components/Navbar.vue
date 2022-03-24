d11fe8b042a5d12c1aeab11f06f3687b95473aa1
MaryahKim <maryapitaz@gmail.com>
2022-03-24 19:03:56 +0300

    Add Login and Sign Up elements

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,19 +4,22 @@
 
 <template>
   <header>
-      <div class=" font-['Poppins'] weight-medium text-grayish-violet flex p-10">
-        <div class="">
-           <img class="" src="../assets/images/logo.svg" alt="logo">
+      <div class=" max-w-full ml-20 font-['Poppins'] weight-medium text-grayish-violet flex  pb-5">
+        <div class=" pr-8">
+           <img class="w-32 " src="../assets/images/logo.svg" alt="logo">
         </div>
-        <nav class="">
+       <div class="w-full flex ">
+          <nav class=" w-full ">
           <RouterLink class="p-3 hover:text-very-dark-violet" to='/'>Features</RouterLink>
           <RouterLink class="p-3 hover:text-very-dark-violet" to= '/pricing' >Pricing</RouterLink>
           <RouterLink class="p-3 hover:text-very-dark-violet" to= '/resources' >Resources</RouterLink>
+
+            <div class=" float-right">
+            <span class=" pr-10">Login</span>
+            <button class="text-zinc-100 text-md px-6 py-3 bg-cyan rounded-3xl">Sign Up</button>
+       </div>
         </nav>
-        <div class="pl-60 flex flex-row items-center">
-            <span class="pr-6">Login</span>
-            <button class="text-zinc-100 text-md px-4 py-2 bg-cyan rounded-3xl">Sign Up</button>
-          </div>
+       </div>
       </div>
   </header>
 </template>
