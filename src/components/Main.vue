c718f63aeca9c3227c82ad907463255943676b5b
MaryahKim <maryapitaz@gmail.com>
2022-03-24 19:03:19 +0300

    Styled P element

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,14 +5,14 @@ export default {
 </script>
 
 <template>
- <div class=" grid grid-cols-2 w-screen pb-10">
-      <div class="p-10  "> 
-        <h1 class=" pb-2 font-black text-5xl  text-very-dark-blue">More than just shorter links</h1>
-        <p class="pb-5 text-grayish-violet">Build your brand's recognition and get detailed insights on how your links are performing.</p>
-        <button class="bg-cyan p-3 rounded-3xl text-white text-xl font-bold "><span class="p-4"> Get Started</span></button>
+ <div class=" grid grid-cols-2 max-w-full pb-10 ">
+      <div class="ml-20 font-['Poppins'] tracking-normal "> 
+        <h1 class="pb-2  font-bold  text-5xl leading-tight text-very-dark-blue">More than just <br> shorter links</h1>
+        <p class="pb-8 leading-8 text-grayish-violet">Build your brand's recognition and get detailed <br> insights on how your links are performing.</p>
+        <button class="bg-cyan px-6 py-3 rounded-3xl text-white text-xl font-bold "><span class="p-4"> Get Started</span></button>
     </div>
-    <div class="p-1 ">
-        <img src="../assets/images/illustration-working.svg" alt="">
+    <div class="pb-20 w-full pr-10">
+        <img class=" " src="../assets/images/illustration-working.svg" alt="">
     </div>
  </div>
 </template>
