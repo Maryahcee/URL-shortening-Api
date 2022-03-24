c9091a61b5daa1fd36c7f0ac83a5c8a98a78bb75
MaryahKim <maryapitaz@gmail.com>
2022-03-24 19:01:55 +0300

    Center Form container
    Added transform styles to Form card

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,10 +7,12 @@ export default {
 </script>
 
 <template>
-  <div class="w-1/2 bg-cover  bg-no-repeat  bg-shorten-desktop bg-very-dark-violet p-5 rounded-md flex items-center justify-center m-auto">
-      <form action="">
-        <input class="bg-white" placeholder="Shorten a link here..." type="link">
-        <button class="w-32  rounded-md p-3">Shorten it!</button>
+  <div class="card transform translate-y-16 font-['Poppins'] w-full bg-cover  bg-no-repeat  bg-shorten-desktop bg-very-dark-violet p-10 rounded-lg flex justify-center align-middle items-center mx-auto">
+      <form action="" class="flex w-full align-middle  justify-center flex-row ">
+        <input class="bg-white w-3/4 rounded-lg pl-5" placeholder="Shorten a link here..." type="link">
+        <div class="pl-5">
+          <button class="w-32 text-zinc-200 text-md   bg-cyan rounded-md p-3">Shorten it!</button>
+        </div>
       </form>
   </div>
 </template>
