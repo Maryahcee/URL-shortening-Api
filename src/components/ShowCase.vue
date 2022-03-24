265ceef9a4da165fcc3ec31d869a46951aaaf984
MaryahKim <maryapitaz@gmail.com>
2022-03-24 19:04:49 +0300

    Create ShowCase.vue
    Create Showcase component.
    Create 3 cards in a container

---
+++ src/components/ShowCase.vue
@@ -0,0 +1,67 @@
+
+
+<script>
+export default {
+
+}
+</script>
+
+<template>
+<!--ShowCase Section with Statistics -->
+
+  <!--Header section -->
+  <div class="font-['Poppins'] bg-zinc-200 w-screen flex flex-col justify-center  items-center pb-10 ">
+      <div class="text-center">
+              <h2 class=" text-2xl font-bold text-very-dark-blue pt-40">Advanced Statistics</h2>
+          <p class="text-grayish-violet pb-20">Track how your links perfoming across the web with <br> our adcanced statistics dashboard</p>
+      </div>
+    <!-- -->
+
+    <div class="flex flex-row  pl-20 w-full">
+          <!-- First Icon Card-->
+        <div class="box-border p-5 mr-5 flex  flex-col  justify-center  items-start h-96 w-96 bg-slate-50 rounded-md">
+          <!--Rounded icon background -->
+            <div class="relative">
+              <div class="transform -translate-y-32 translate-x-4  bg-dark-violet rounded-full w-20 h-20 flex items-center">
+                <!--Brand Recognition icon  -->
+                <img class="w-10 flex items-center justify-center mx-auto align-middle" src="../assets/images/icon-brand-recognition.svg" alt="">
+              </div>
+            </div>
+            <h3 class="text-xl font-bold pb-1 text-very-dark-violet">Brand Recognition</h3>
+            <p class="text-grayish-violet pb-5">Boost your brand recognition with each click. Generic links don’t 
+                mean a thing. Branded links help instil confidence in your content.
+            </p>
+        </div>
+
+        <!-- Second Icon Card-->
+        <div class="box-border p-5 mr-5 mt-10  h-96 w-96 bg-slate-50 rounded-md">
+          <!--Detailed Records icon background -->
+            <div class="relative">
+              <div class="transform -translate-y-16 translate-x-4  bg-dark-violet rounded-full w-20 h-20 flex items-center">
+                <!--Detailed Records icon  -->
+                <img class="w-10 flex items-center justify-center mx-auto align-middle" src="../assets/images/icon-detailed-records.svg" alt="">
+              </div>
+            </div>
+            <h3 class="text-xl font-bold pb-1 text-very-dark-violet">Detailed Records</h3>
+            <p class="text-grayish-violet pb-5">Gain insights into who is clicking your links. Knowing when and where 
+                people engage with your content helps inform better decisions.
+            </p>
+        </div>
+
+        <!-- Third Icon Card-->
+        <div class="box-border p-3 mt-20 flex  flex-col  justify-center  items-start h-96 w-96 bg-slate-50 rounded-md">
+          <!--Rounded icon background -->
+            <div class="relative">
+              <div class="transform -translate-y-32 translate-x-4  bg-dark-violet rounded-full w-20 h-20 flex items-center">
+                <!--Fully Customizable icon  -->
+                <img class="w-10 flex items-center justify-center mx-auto align-middle" src="../assets/images/icon-brand-recognition.svg" alt="">
+              </div>
+            </div>
+            <h3 class="text-xl font-bold pb-1 text-very-dark-violet">Fully Customizable</h3>
+            <p class="text-grayish-violet pb-5">Improve brand awareness and content discoverability through customizable 
+                links, supercharging audience engagement.
+            </p>
+        </div>
+    </div>
+  </div>
+</template>
