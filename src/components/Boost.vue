5b73ebc3e987f3f12e8389f866334a91e0746d84
MaryahKim <maryapitaz@gmail.com>
2022-04-21 11:11:42 +0300

    Update Boost.vue
    change button background color

--- src/components/Boost.vue
+++ src/components/Boost.vue
@@ -2,7 +2,7 @@
   <div class="card max-w-full">
     <div class="p-10 max-w-full flex flex-col items-center justify-center bg-shorten-desktop bg-no-repeat  bg-cover bg-dark-violet font-['Poppins']">
         <h3 class=" text-zinc-200  text-center text-4xl font-extrabold pb-5 ">Boost your links today</h3>
-        <button class="bg-cyan text-zinc-200 text-2xl font-semibold  rounded-full py-3 px-6">Get Started</button>
+        <button class="bg-Cyan text-zinc-200 text-2xl font-semibold  rounded-full py-3 px-6">Get Started</button>
     </div>
   </div>
 </template>
