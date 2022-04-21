4b444e6fef74216ae45981f9ebd16dc2d7ce235f
MaryahKim <maryapitaz@gmail.com>
2022-04-21 11:15:51 +0300

    Update main.js
    import store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import { createApp } from 'vue'
+import { createApp, VueElement } from 'vue'
+import VueClipboard from 'vue-clipboard2'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+
+//import store
+import store from './store/index'
 
 const app = createApp(App)
 
 app.use(router)
 
+app.use(store)
+
+app.use(VueClipboard)
+
 app.mount('#app')
+
+
