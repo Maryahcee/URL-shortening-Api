aa5e690506b5d365d4759dcb10c084dfb65e6a18
MaryahKim <maryapitaz@gmail.com>
2022-04-21 11:12:37 +0300

    Update Form.vue
    add a method to call link shortener api

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,17 +2,59 @@
 
 <script>
 export default {
-
+  
+  methods: {
+    showLinks(url) {
+      if(url === undefined){
+        this.$store.dispatch('setError', 'Empty error')
+      }
+      else if(!url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g))
+      {
+        this.$store.dispatch('setError', 'Invalid url')
+      }
+      else{
+        ///
+        this.$store.dispatch('setError','')
+        this.$store.dispatch('getUrls', url)
+      }
+      
+    },
+    
+  }
 }
+
 </script>
 
 <template>
-  <div class="card transform translate-y-16 font-['Poppins'] w-full bg-cover  bg-no-repeat  bg-shorten-desktop bg-very-dark-violet p-10 rounded-lg flex justify-center align-middle items-center mx-auto">
-      <form action="" class="flex w-full align-middle  justify-center flex-row ">
-        <input class="bg-white w-3/4 rounded-lg pl-5" placeholder="Shorten a link here..." type="link">
-        <div class="pl-5">
-          <button class="w-32 text-zinc-200 text-md   bg-cyan rounded-md p-3">Shorten it!</button>
+<!-- url link -->
+<div class="container bg-zinc-200 max-w-full font-['Poppins']"
+>
+   <div class="container  mx-auto w-3/4  ">
+      <div class=" transform -translate-y-16 w-full bg-cover  bg-no-repeat  bg-shorten-desktop bg-very-dark-violet p-10 rounded-lg">
+        <form @submit.prevent="showLinks(url)" class="flex w-full align-middle  justify-center flex-row ">
+          <input class="bg-white w-3/4 rounded-lg pl-5" 
+            placeholder="Shorten a link here... " 
+            type="url"
+            v-model="url"
+           > 
+
+          <div class="pl-5">
+            <button @click="[isActive = !isActive]"
+             class="w-32 text-zinc-200 text-md  bg-Cyan rounded-md p-3"
+             :class="[isActive ? 'bg-Cyan' : 'bg-rose-300']"
+             type="submit"
+            >{{ isActive ? 'Shorten it': 'Shorten  it ' }}</button>
+          </div>
+        </form>
+        <!-- url link error message -->
+        <div>
+          <h1 
+            class="text-red-500 ml-14 italic">
+              {{ $store.state.error }} 
+          </h1>
         </div>
-      </form>
-  </div>
+      </div>
+   </div>
+</div>
+   
 </template>
