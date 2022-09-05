28841584d5fcb680dffb6c1ec540e5e55ddd6339
Mary Njoroge <maryapitaz@gmail.com>
2022-09-05 18:37:18 +0300

    add margins responiveness (#1)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,8 @@
       text-white
       font-['Poppins']
       container
-      flex
+      hidden
+      md:flex
       items-center
       justify-center
       pt-10
@@ -28,27 +29,27 @@
           <div class="card mt-0 m-10">
             <h1 class="pb-3 font-extrabold text-lg">Features</h1>
             <ul class="text-gray text-normal text-md">
-              <li class="hover:text-cyan">Link Shortening</li>
-              <li class="hover:text-cyan">Branded Links</li>
-              <li class="hover:text-cyan">Analytics</li>
+              <li class="hover:text-Cyan">Link Shortening</li>
+              <li class="hover:text-Cyan">Branded Links</li>
+              <li class="hover:text-Cyan">Analytics</li>
             </ul>
           </div>
           <!--Resources List-->
           <div class="card mt-0 m-10">
             <h1 class="pb-3 font-extrabold text-lg">Resources</h1>
             <ul class="text-gray text-normal text-md">
-              <li class="hover:text-cyan">Blog</li>
-              <li class="hover:text-cyan">Developers</li>
-              <li class="hover:text-cyan">Support</li>
+              <li class="hover:text-Cyan">Blog</li>
+              <li class="hover:text-Cyan">Developers</li>
+              <li class="hover:text-Cyan">Support</li>
             </ul>
           </div>
           <!--Resources List-->
           <div class="card mt-0 m-10">
             <h1 class="pb-3 font-extrabold text-lg">Company</h1>
             <ul class="text-gray text-normal text-md">
-              <li class="hover:text-cyan">About</li>
-              <li class="hover:text-cyan">Our Team</li>
-              <li class="hover:text-cyan">Careers</li>
+              <li class="hover:text-Cyan">About</li>
+              <li class="hover:text-Cyan">Our Team</li>
+              <li class="hover:text-Cyan">Careers</li>
             </ul>
           </div>
           <!-- Interactive social Media icons-->
@@ -65,22 +66,22 @@
                 "
               >
                 <img
-                  class="pl-10"
+                  class="hover:bg-Cyan pl-10"
                   src="../assets/images/icon-facebook.svg"
                   alt="Facebook"
                 />
                 <img
-                  class="pl-5"
+                  class="hover:bg-Cyan pl-5"
                   src="../assets/images/icon-twitter.svg"
                   alt="twitter"
                 />
                 <img
-                  class="pl-5"
+                  class="hover:bg-Cyan pl-5"
                   src="../assets/images/icon-pinterest.svg"
                   alt="pinterest"
                 />
                 <img
-                  class="pl-5"
+                  class="hover:bg-Cyan pl-5"
                   src="../assets/images/icon-instagram.svg"
                   alt="instagram"
                 />
@@ -100,11 +101,86 @@
       </div>
     </div>
   </footer>
+  <!--Mobile Footer-->
+  <div class="bg-very-dark-blue text-white">
+    <!--Mobile footer logo-->
+    <div class="card flex justify-center mt-8 w-full">
+      <h1 class="text-white font-extrabold text-4xl">Shortly</h1>
+    </div>
+
+    <!--Mobile Features List-->
+    <div class="md:hidden flex flex-col justify-center items-center mx-auto">
+      <div class="mt-10 m-5">
+        <h1 class="pb-3 font-extrabold text-lg text-center">Features</h1>
+        <ul
+          class="
+            text-gray text-normal text-md
+            justify-center
+            items-center
+            flex flex-col
+          "
+        >
+          <li class="hover:text-Cyan">Link Shortening</li>
+          <li class="hover:text-Cyan">Branded Links</li>
+          <li class="hover:text-Cyan">Analytics</li>
+        </ul>
+      </div>
+      <!--Mobile Resources List-->
+      <div class="mt-0 m-5">
+        <h1 class="pb-3 font-extrabold text-lg text-center">Resources</h1>
+        <ul
+          class="
+            text-gray text-normal text-md
+            justify-center
+            items-center
+            flex flex-col
+          "
+        >
+          <li class="hover:text-Cyan">Blog</li>
+          <li class="hover:text-Cyan">Developers</li>
+          <li class="hover:text-Cyan">Support</li>
+        </ul>
+      </div>
+      <!--Mobile Resources List-->
+      <div class="mt-0 m-5">
+        <h1 class="pb-3 font-extrabold text-lg text-center">Company</h1>
+        <ul
+          class="
+            text-gray text-normal text-md
+            justify-center
+            items-center
+            flex flex-col
+          "
+        >
+          <li class="hover:text-Cyan">About</li>
+          <li class="hover:text-Cyan">Our Team</li>
+          <li class="hover:text-Cyan">Careers</li>
+        </ul>
+      </div>
+      <!--Mobile Interactive social Media icons-->
+      <div class="flex flex-row mx-auto justify-center items-center mb-5">
+        <img
+          class="items-center"
+          src="../assets/images/icon-facebook.svg"
+          alt="Facebook"
+        />
+        <img
+          class="pl-5"
+          src="../assets/images/icon-twitter.svg"
+          alt="twitter"
+        />
+        <img
+          class="pl-5"
+          src="../assets/images/icon-pinterest.svg"
+          alt="pinterest"
+        />
+        <img
+          class="pl-5"
+          src="../assets/images/icon-instagram.svg"
+          alt="instagram"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {};
-</script>
-
-<style>
-</style>
--- src/components/ShowCase.vue
+++ src/components/ShowCase.vue
@@ -3,7 +3,7 @@
   <!--ShowCase Section with Statistics -->
 
   <!--Header section -->
-  <div class="max-w-full container bg-zinc-200">
+  <div class="max-w-full hidden md:flex container bg-zinc-200">
     <div
       class="
         font-['Poppins']
@@ -152,6 +152,126 @@
           </p>
         </div>
       </div>
+    </div>
+  </div>
+  <!--Mobile Version statistic section-->
+  <div class="text-center">
+    <h2 class="text-2xl font-bold text-very-dark-blue pt-5">
+      Advanced Statistics
+    </h2>
+    <p class="text-grayish-violet pb-10">
+      Track how your links are performing across the web with our advanced
+      statistics dashboard.
+    </p>
+  </div>
+  <!-- Mobile Icons -->
+  <div class="md:hidden flex flex-col items-center mx-auto w-full">
+    <!--Mobile First Icon Card-->
+    <div class="box-border mt-10 p-5 mr-5 w-3/4 bg-slate-50 rounded-md">
+      <!-- Mobile Circular background -->
+      <div class="relative flex items-center justify-center">
+        <div
+          class="
+            transform
+            -translate-y-16
+            bg-dark-violet
+            rounded-full
+            w-20
+            h-20
+            flex
+            items-center
+            justify-center
+          "
+        >
+          <!-- Mobile Brand Recognition icon  -->
+          <img
+            class="w-10 flex items-center justify-center mx-auto align-middle"
+            src="../assets/images/icon-brand-recognition.svg"
+            alt=""
+          />
+        </div>
+      </div>
+      <h3
+        class="text-xl -mt-5 font-bold pb-1 text-very-dark-violet text-center"
+      >
+        Brand Recognition
+      </h3>
+      <p class="text-grayish-violet pb-5 text-center">
+        Boost your brand recognition with each click. Generic links don’t mean a
+        thing. Branded links help instil confidence in your content.
+      </p>
+    </div>
+
+    <!--Mobile Second Icon Card-->
+    <div class="box-border p-5 mr-5 mt-16 w-3/4 bg-slate-50 rounded-md">
+      <!-- Mobile Circular background -->
+      <div class="relative flex items-center justify-center">
+        <div
+          class="
+            transform
+            -translate-y-16
+            bg-dark-violet
+            rounded-full
+            w-20
+            h-20
+            flex
+            items-center
+            justify-center
+          "
+        >
+          <!-- Mobile Detailed Records icon  -->
+          <img
+            class="w-10 flex items-center justify-center mx-auto align-middle"
+            src="../assets/images/icon-detailed-records.svg"
+            alt=""
+          />
+        </div>
+      </div>
+      <h3
+        class="text-xl -mt-5 font-bold pb-1 text-very-dark-violet text-center"
+      >
+        Detailed Records
+      </h3>
+      <p class="text-grayish-violet text-center">
+        Gain insights into who is clicking your links. Knowing when and where
+        people engage with your content helps inform better decisions.
+      </p>
+    </div>
+
+    <!--Mobile Third Icon Card-->
+    <div class="box-border p-5 mt-16 mb-10 w-3/4 bg-slate-50 rounded-md">
+      <!-- Mobile Circular background -->
+      <div class="relative flex items-center justify-center">
+        <div
+          class="
+            transform
+            -translate-y-16
+            bg-dark-violet
+            rounded-full
+            w-20
+            h-20
+            flex
+            items-center
+            justify-center
+          "
+        >
+          <!--Mobile Fully Customizable icon  -->
+          <img
+            class="w-10 flex items-center justify-center mx-auto align-middle"
+            src="../assets/images/icon-brand-recognition.svg"
+            alt=""
+          />
+        </div>
+      </div>
+      <h3
+        class="text-xl -mt-5 font-bold pb-1 text-very-dark-violet text-center"
+      >
+        Fully Customizable
+      </h3>
+      <p class="text-grayish-violet pb-5 text-center">
+        Improve brand awareness and content discoverability through customizable
+        links, supercharging audience engagement.
+      </p>
     </div>
   </div>
 </template>
